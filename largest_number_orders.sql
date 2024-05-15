372ce64e38cf3224fc530adfec136fb7b63d1248
OWAIS C H <61494102+owais-ch@users.noreply.github.com>
2024-05-15 14:12:12 +0530

    Create largest_number_orders.sql

---
+++ largest_number_orders.sql
@@ -0,0 +1,43 @@
+/*
+Customer Placing the Largest Number of Orders (LeetCode)
+Table: Orders
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key (column with unique values) for this table.
+This table contains information about the order ID and the customer ID.
+ 
+
+Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+*/
+
+SELECT customer_number
+FROM ((SELECT COUNT(customer_number) AS sum_customer,customer_number FROM Orders GROUP BY customer_number ORDER BY sum_customer DESC) LIMIT 1) AS TABLE1
