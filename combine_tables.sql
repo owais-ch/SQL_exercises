1c33dfc6d58ced17f3f771af989a690005e3611d
OWAIS C H <61494102+owais-ch@users.noreply.github.com>
2024-06-18 11:05:37 +0530

    Create combine_tables.sql

---
+++ combine_tables.sql
@@ -0,0 +1,40 @@
+/*
+Combine Two Tables (LeetCode)
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
+*/
+
+SELECT p1.firstName AS firstName,
+       p1.lastName AS lastName,
+       COALESCE(p2.city,NULL) AS city,
+       COALESCE(p2.state,NULL) AS state
+FROM Person p1
+LEFT JOIN Address p2
+ON p1.personId=p2.personId
